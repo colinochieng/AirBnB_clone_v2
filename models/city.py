2410e40c2e674b03ee4071d18876f04085edf3e4
colinochieng <colinokumu89@gmail.com>
2023-07-22 13:04:08 +0300

    hbnb

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from os import environ
 
 
@@ -11,6 +12,8 @@ class City(BaseModel, Base):
     if environ['HBNB_TYPE_STORAGE'] == 'db':
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        cas_str = 'all, delete, delete-orphan'
+        places = relationship('Place', backref='cities', cascade=cas_str)
     else:
         state_id = ""
         name = ""
