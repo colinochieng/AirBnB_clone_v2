cfd90f26db0fbb9bfb0815227a775b1b4e152872
colinochieng <colinokumu89@gmail.com>
2023-07-24 01:55:26 -0700

    sqlalchemy

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
 
